893d6a4d52a183624da2a404a980cdf147572f69
Fredrik Johansson <f95johansson@gmail.com>
2017-05-19 15:21:31 +0200

    Added autoplay

--- src/components/Player.js
+++ src/components/Player.js
@@ -9,10 +9,16 @@ export default class Player extends Component {
     this.search = this.search.bind(this);
     this.keyPress = this.keyPress.bind(this);
     this.nextSong = this.nextSong.bind(this);
+    this.startPlaying = this.startPlaying.bind(this);
+    this.autoplay = this.autoplay.bind(this);
+    this.setAudioPlayer = this.setAudioPlayer.bind(this);
+    
 
     this.state = {track: null};
     this.tracks = [];
     this.history = {};
+    this.playButtonIntervalId = null;
+    this.audioPlayer = null;
 
     document.addEventListener('keydown', this.keyPress);
   }
@@ -30,6 +36,7 @@ export default class Player extends Component {
   }
 
   nextSong() {
+    console.log(this.tracks.length);
     if (this.tracks.length === 0) {
       new LastFM().getSimilar(this.state.track.trackName, this.state.track.artist)
         .then((tracks) => {
@@ -64,19 +71,51 @@ export default class Player extends Component {
       .catch(errorMessage => console.log('Error: ', errorMessage));
   }
 
+  setAudioPlayer(player) {
+    this.audioPlayer = player;
+  }
+
   startPlaying(iframe) {
-    document.getElementById('play-button').click();
+    let playButton = iframe.target.contentWindow.document.getElementById('play-button');
+    if (playButton !== null) {
+      playButton.click();
+    }
+  }
+
+  startPlaying2() {
+    let playButton = document.getElementById('play-button');
+    if (playButton !== null) {
+      playButton.click();
+      clearInterval(this.playButtonIntervalId);
+    }
+  }
+
+  autoplay() {
+    this.playButtonIntervalId = setInterval(this.startPlaying2, 500);
   }
 
   render() {
+    if (this.audioPlayer !== null) {
+      if (this.state.track.audioPreviewUrl === null) {
+        this.nextSong();
+      } else {
+        this.audioPlayer.src = this.state.track.audioPreviewUrl;
+        this.audioPlayer.play();
+      }
+    }
     return (
         <div>   
           {this.state.track === null ?
               <SearchField onSearch={this.search} /> :
-              <iframe src={'https://open.spotify.com/embed?uri=spotify:track:'+this.state.track.spotifyId}
+              /*<iframe src={'https://open.spotify.com/embed?uri=spotify:track:'+this.state.track.spotifyId}
                       frameBorder="0" allowTransparency="true"
-                      ref={this.startPlaying}>
-              </iframe>}
+                      width="288"
+                      onLoad={this.startPlaying}>
+              </iframe>*/
+              <audio controls autoPlay ref={this.setAudioPlayer}>
+                <source src={this.state.track.audioPreviewUrl} type="audio/mpeg"/>
+                Your browser does not support the audio element.
+              </audio>}
         </div>
       );
   }
